c5f397d8e620acaafa5ed1c977163a4b555d119e
Robin Kurosawa <rkuro@bu.edu>
2019-09-22 14:11:37 -0400

    i hate computers

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
+    console.log("something is fucking up")
   res.send('Express/Nginx/Certbot tutorial');
 });
 
