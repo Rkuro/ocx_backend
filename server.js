bc98da57d8bcb67f183e60b594a9ec4d0b4eccd6
Robin Kurosawa <rkuro@bu.edu>
2019-09-22 15:49:00 -0400

    fuck me

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get('/', (req, res) => {
     console.log("something is fucking up")
-  res.send('Express/Nginx/Certbot tutorial');
+    res.send('Express/Nginx/Certbot tutorial');
 });
 
-app.listen(3000);
+app.listen(80);
